6fd95360674a02c4aecdd6310aa0646be03626ef
skittWhatIsThat <Oleg.skitt@mail.ru>
2021-06-25 18:37:50 +0300

    get controller

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -8,6 +8,21 @@ module Api
         render json: Account.all
       end
 
+      def create
+        account = Account.new(message_params)
+        if account.save
+          render json: account, status: :created
+        else
+          render json: account.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def message_params
+        params.require(:account).permit(:login, :password)
+      end
+
     end
   end
 end
