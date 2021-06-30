f1b02db8bd59d39b65309d7576c2d902b07422ba
skittWhatIsThat <Oleg.skitt@mail.ru>
2021-06-30 11:27:51 +0300

    add webhook with interact

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -9,23 +9,6 @@ module Api
       end
 
       def create
-
-        def index
-          @webhook = Webhook.new
-          @number_of_emails_sent = @webhook.number_of_events('send')
-          @number_of_emails_opened = @webhook.number_of_events('open')
-          @number_of_clicks = @webhook.number_of_events('click')
-          @email_types = @webhook.email_types
-        end
-
-        def create
-          data = JSON.parse(request.body.read)
-          Webhook.create(address: data['Address'],
-                         email_type: data['EmailType'],
-                         event: data['Event'],
-                         timestamp: data['Timestamp'])
-          head 200
-          end
         account = Account.new(message_params)
         if account.save
           render json: account, status: :created
